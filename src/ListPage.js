4b31e2631830a1693e16cb4da661e1f6fbf1c245
joshua360x <jkw5499@psu.edu>
2022-02-09 19:04:51 -0500

    added Link to each game for setup of Details page

--- src/ListPage.js
+++ src/ListPage.js
@@ -4,10 +4,18 @@ import Game from './Game';
 
 export default function ListPage() {
   // you'll need some state to hold onto the array of games
+  const [games, setGames] = useState([]);
 
   // fetch the games on load and inject them into state
+  useEffect(() => {
+    async function onLoad() {
+      const data = await getGames();
+
+    }
+    onLoad();
+  }, []);
   return (
-    <div className='list games'>
+    <div className="list games">
       {/* map through the games in state and render Game components */}
     </div>
   );
